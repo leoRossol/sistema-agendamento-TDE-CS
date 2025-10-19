9ec2136b7dd0af32d780256a0e7c1b520c56c35f
magotiti <cardosoth1909@gmail.com>
2025-10-19 19:22:22 -0300

    US19/initial-structure-without-tests

---
+++ src/main/java/com/sistema/agendamento/sistema_agendamento/service/AutenticacaoService.java
@@ -0,0 +1,21 @@
+package com.sistema.agendamento.sistema_agendamento.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.sistema.agendamento.sistema_agendamento.entity.Usuario;
+import com.sistema.agendamento.sistema_agendamento.repository.UsuarioRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Service
+public class AutenticacaoService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); // armazenamos somente o encoder, nao a senha em si
+
+    public Usuario login(String email, String senha) {
+        Usuario usuario = usuarioRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+        if (!passwordEncoder.matches(senha, usuario.getSenha())) throw new RuntimeException("Senha incorreta");
+        
+        return usuario;
+    }
+}
