36f4678f02a19df54113e8c9f6b7a7a67dfb2a92
LeoSimonn <leonardo.monteiro004@edu.pucrs.br>
2025-10-27 21:17:30 -0300

    Fix: Conflito GlobalExceptionHandler resolvido, SistemaAgendamentoApplication criado

--- src/main/java/com/sistema/agendamento/sistema_agendamento/SistemaAgendamentoApplication.java
+++ src/main/java/com/sistema/agendamento/sistema_agendamento/SistemaAgendamentoApplication.java
@@ -2,8 +2,10 @@ package com.sistema.agendamento.sistema_agendamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SistemaAgendamentoApplication {
 
 	public static void main(String[] args) {
